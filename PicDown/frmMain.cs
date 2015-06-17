df334f7bbe5112f0565f2f5a23ff110282dc71e7
ricekeller <ricekeller@gmail.com>
2015-06-17 15:48:29 -0500

    created
    created

---
+++ PicDown/frmMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicDown
+{
+	public partial class frmMain : Form
+	{
+		public string RootURL { get; set; }
+		public int StartIdx { get; set; }
+		public int EndIdx { get; set; }
+		public string Prefix { get; set; }
+		public string Suffix { get; set; }
+
+		
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+	}
+}
